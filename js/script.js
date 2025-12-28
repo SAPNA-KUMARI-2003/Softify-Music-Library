b5b03682523ab6121c66c21f6deaf27c2abf729f
Sapna Kumari <sapnakumari702352@email.com>
2025-12-28 11:41:50 +0530

    songs added

--- js/script.js
+++ js/script.js
@@ -93,7 +93,7 @@ async function getSongs(folder) {
                                      <div class="song-name">${decodeURIComponent(
                                        safeName
                                      )}</div>
-                                     <div>Sapna</div>
+                                     <div></div>
                                  </div>
                                  <div class="playnow">
                                      <img  class="invert " src="img/play.svg" alt="">
